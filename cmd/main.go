81c7e42bd00e9aef6ee4aaf9dfdc4bc6d944aeff
Vickey Shrestha <logontovickey@hotmail.com>
2018-07-20 00:26:32 -0500

    - introduced mongoDB Driver

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Person struct {
+	Name string
+	Phone string
+}
 
 func main() {
-	fmt.Println("Hello World")
+	mongoDBURL:= "192.168.202.131:27017"
+	session, err := mgo.Dial(mongoDBURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
 }
