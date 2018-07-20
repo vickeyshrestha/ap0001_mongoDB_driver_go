a5d39b2ae6e9e712dc35ebe5522a40c6f975a6c6
Vickey Shrestha <logontovickey@hotmail.com>
2018-07-20 14:38:00 -0500

    - Introduced Godog

--- behaviorDrivenTest/testMongoDriver_test.go
+++ behaviorDrivenTest/testMongoDriver_test.go
@@ -3,7 +3,7 @@ package behaviorDrivenTest_test
 import "github.com/DATA-DOG/godog"
 
 func thereAreGodogs(arg1 int) error {
-return godog.ErrPending
+	return godog.ErrPending
 }
 
 func iEat(arg1 int) error {
@@ -19,4 +19,3 @@ func FeatureContext(s *godog.Suite) {
 	s.Step(`^I eat (\d+)$`, iEat)
 	s.Step(`^there should be (\d+) remaining$`, thereShouldBeRemaining)
 }
-
