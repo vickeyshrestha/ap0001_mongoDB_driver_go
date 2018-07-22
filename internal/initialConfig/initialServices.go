d89ac9bc1bb03b4fae052506497d8cf7243097af
Vickey Shrestha <logontovickey@hotmail.com>
2018-07-22 12:16:42 -0500

    - Added new initial config files

--- internal/initialConfig/initialServices.go
+++ internal/initialConfig/initialServices.go
@@ -7,3 +7,29 @@ func GetApplicationSite() string {
 	appSite = strings.Join(configFromJsonFile.Site,"")
 	return appSite
 }
+
+func GetApplicationBinary() string {
+	var appBinary string
+	appBinary = strings.Join(configFromJsonFile.BinaryVersion,"")
+	return appBinary
+}
+
+func GetMongoDBEndpoint() string {
+	var mongoEndpoint string
+	mongoEndpoint = strings.Join(configFromJsonFile.MongoDbEndpoint,"")
+	return mongoEndpoint
+}
+
+func GetMongoDBPort() string {
+	var mongoPort string
+	mongoPort = strings.Join(configFromJsonFile.MongoDbPort,"")
+	return mongoPort
+}
+
+func GetHttpConnectionTimeout() string {
+	var httpConnectionTimeout string
+	httpConnectionTimeout = strings.Join(configFromJsonFile.HTTPConnectionTimeout,"")
+	return httpConnectionTimeout
+}
+
+
