a0d90984b1a7c12fd7b4acd9d0899691ff2e9fd2
Vickey Shrestha <logontovickey@hotmail.com>
2018-07-23 18:29:51 -0500

    - added a readme file

--- internal/mongoAdapter/adapter.go
+++ internal/mongoAdapter/adapter.go
@@ -9,35 +9,6 @@ import (
 	"io/ioutil"
 )
 
-/*
-func MongoAdapterTest() {
-	var mongoDbURL = initialConfig.GetMongoDBEndpoint() + ":" + initialConfig.GetMongoDBPort()
-	session, err := mgo.Dial(mongoDbURL)
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
-	c := session.DB("test").C("people")
-	err = c.Insert(Person{"Ale", "+55 53 8116 9639"},
-		Person{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result := Person{}
-	err = c.Find(bson.M{"name": "Ale"}).One(&result)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Phone:", result.Phone)
-}
-*/
-
 type Server struct {
 	session *mgo.Session
 }
