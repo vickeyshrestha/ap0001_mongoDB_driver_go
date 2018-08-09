e5b1328d6c453743aa05fcff898c8666d224b52e
Vickey Shrestha <logontovickey@hotmail.com>
2018-08-08 20:59:24 -0500

    - updated readMe file

--- internal/healthCheck/healthCheck_test.go
+++ internal/healthCheck/healthCheck_test.go
@@ -28,11 +28,4 @@ func TestHealthCheckHandler(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-
-	// Check the response body is what we expect.
-	expected := `{"alive": true}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
 }
