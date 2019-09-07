6ff7600b564ccf1dfcfcb707d6dedae3e70fb2b0
Vickey Shrestha <logontovickey@hotmail.com>
2019-09-07 13:12:16 -0500

    - code refactor

--- internal/applicationDriver/controllers.go
+++ internal/applicationDriver/controllers.go
@@ -1,7 +1,49 @@
 package applicationDriver
 
-import "github.com/gorilla/mux"
+import (
+	"ap0001_mongo_engine"
+	"ap0001_mongo_engine/internal/healthCheck"
+	"ap0001_mongo_engine/internal/mongoAdapter"
+	"github.com/gorilla/mux"
+	"gopkg.in/tylerb/graceful.v1"
+	"net/http"
+	"time"
+)
 
-func routes(request *mux.Router) {
+type Service struct {
+	mongo  mongoAdapter.Server
+	health healthCheck.Service
+}
 
+func NewService(mongoServer mongoAdapter.Server, healthServer healthCheck.Service) *Service {
+	return &Service{
+		mongo:  mongoServer,
+		health: healthServer,
+	}
+}
+
+func (s *Service) Routes(request *mux.Router) *graceful.Server {
+	// example: http://localhost:8085/health
+	request.HandleFunc(ap0001_mongo_engine.HealthCheck, s.health.HealthCheckHandler).Methods("GET")
+
+	// example: http://localhost:8085/getallconfigs
+	request.HandleFunc(ap0001_mongo_engine.GetAllConfigsFromDatabase, s.mongo.GetClientConfigAll).Methods("GET")
+
+	// example http://localhost:8085/getconfig?app=testApplication&bin=0.0.2&site=dev
+	request.HandleFunc(ap0001_mongo_engine.GetSingleConfig, s.mongo.GetClientConfigBasedOnAppNameAndBinaryVersionAndSite).Methods("GET")
+
+	// example http://localhost:8085/insertnew
+	request.HandleFunc(ap0001_mongo_engine.InsertConfig, s.mongo.InsertNewConfig).Methods("POST")
+
+	// example http://localhost:8085/delete?app=testApplication&bin=0.0.2&site=dev
+	request.HandleFunc(ap0001_mongo_engine.DeleteConfig, s.mongo.DeleteRecordUsingID).Methods("DELETE")
+
+	server := &graceful.Server{
+		Timeout: 30 * time.Second,
+		Server: &http.Server{
+			Addr:    ":8085",
+			Handler: request,
+		},
+	}
+	return server
 }
--- internal/applicationDriver/driver.go
+++ internal/applicationDriver/driver.go
@@ -7,12 +7,9 @@ import (
 	"ap0001_mongo_engine/internal/mongoAdapter"
 	"fmt"
 	"github.com/gorilla/mux"
-	"gopkg.in/tylerb/graceful.v1"
 	"log"
-	"net/http"
 	"os"
 	"strings"
-	"time"
 )
 
 func Start(config ap0001_mongo_engine.InitialConfig) {
@@ -29,28 +26,9 @@ func Start(config ap0001_mongo_engine.InitialConfig) {
 		if err != nil {
 			panic(err)
 		}
-		// example: http://localhost:8085/health
-		request.HandleFunc(ap0001_mongo_engine.HealthCheck, healthServer.HealthCheckHandler).Methods("GET")
 
-		// example: http://localhost:8085/getallconfigs
-		request.HandleFunc(ap0001_mongo_engine.GetAllConfigsFromDatabase, mongoServer.GetClientConfigAll).Methods("GET")
-
-		// example http://localhost:8085/getconfig?app=testApplication&bin=0.0.2&site=dev
-		request.HandleFunc(ap0001_mongo_engine.GetSingleConfig, mongoServer.GetClientConfigBasedOnAppNameAndBinaryVersionAndSite).Methods("GET")
-
-		// example http://localhost:8085/insertnew
-		request.HandleFunc(ap0001_mongo_engine.InsertConfig, mongoServer.InsertNewConfig).Methods("POST")
-
-		// example http://localhost:8085/delete?app=testApplication&bin=0.0.2&site=dev
-		request.HandleFunc(ap0001_mongo_engine.DeleteConfig, mongoServer.DeleteRecordUsingID).Methods("DELETE")
-
-		server := &graceful.Server{
-			Timeout: 30 * time.Second,
-			Server: &http.Server{
-				Addr:    ":8085",
-				Handler: request,
-			},
-		}
+		server := NewService(*mongoServer, *healthServer)
+		server.Routes(request)
 
 		ip, err := generalUtilities.ExternalIP()
 		if err != nil {
@@ -60,13 +38,13 @@ func Start(config ap0001_mongo_engine.InitialConfig) {
 		log.Printf("Application started successfully. Running in ip %v & serving port 8085", ip)
 		if strings.EqualFold(*config.GetSSLMode(), "false") {
 			log.Printf("Dev mode set to false. Starting application in ssl secured mode")
-			errStartingServer := server.ListenAndServeTLS(*config.GetSslCert(), *config.GetSslKey())
+			errStartingServer := server.Routes(request).ListenAndServeTLS(*config.GetSslCert(), *config.GetSslKey())
 			if errStartingServer != nil {
 				log.Printf("Failed to start server | Error: %v", errStartingServer)
 			}
 		} else {
 			log.Printf("Starting application in ssl non-secured mode")
-			err = server.ListenAndServe()
+			err = server.Routes(request).ListenAndServe()
 			if err != nil {
 				panic(err)
 			}
